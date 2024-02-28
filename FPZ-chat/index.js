7f5cb23e76fa0c3a97ffbd6c65c3a4df3ea0f955
Felpzsouls <87570837+felpzsouls@users.noreply.github.com>
2024-02-28 10:35:23 -0300

    Create index.js

---
+++ FPZ-chat/index.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+    app = express(),
+    { createServer } = require('node:http'),
+    server = createServer(app),
+    { join } = require('node:path'),
+    { Server } = require('socket.io'),
+    io = new Server(server);
+
+
+app.get('/', async (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', async (socket) => {
+
+    socket.broadcast.emit('Oi');
+    socket.on('chat message', async(msg) => {
+      io.emit('chat message', msg);
+    })
+    io.emit('chat message', 'seja bem vindo')
+})
+
+server.listen('80', () => console.log('site online'))
